e13d431c81f62511a94b0c4894b9bf5e108b005d
Dan Valinotti <dvalinotti@icloud.com>
2018-12-09 12:51:40 -0500

    naming fix

--- broker.php
+++ broker.php
@@ -27,6 +27,8 @@ function doPush($destination, $version)
 {
     foreach (ip as $target => $ip) {
         if (endsWith($target, $destination)) {
+            $tg_array = substr('-', $target);
+            $target = $tg_array[0];
             echo "Pushing ver" . $version . " to " . $target . '-' . $destination . "...\n";
             $b = push($target, $destination, $version);
             $fn = $version . 'tar.gz';
