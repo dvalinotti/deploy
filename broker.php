dee74b11027de7b5dd6251f6db246a8beb40c5cf
Dan Valinotti <dvalinotti@icloud.com>
2018-12-14 18:30:01 -0500

    more stuff for site

--- broker.php
+++ broker.php
@@ -3,81 +3,38 @@
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
-require_once('login.php.inc');
-require_once('functions.inc');
 
-
-function doBundle($source, $version)
+function doLogin($username,$password)
 {
-    $result = false;
-    foreach (ip as $target => $ip) {
-        if (endsWith($target, $source)) {
-            echo "BUNDLING:" .  $target . "\n";
-            $b = bundle($source, str_replace('-' . $source, '', $target), $version);
-
-            echo $b . "\n";
-        }
-    }
-
-    return 'BUNDLE COMPLETE';
-}
-
-function doPush($destination, $version)
-{
-    foreach (ip as $target => $ip) {
-        if (endsWith($target, $destination)) {
-            $layer = strtok($target, '-');
-            echo "Pushing ver" . $version . " to " . $layer . '-' . $destination . "...\n";
-            $b = push($layer, $destination, $version);
-
-            echo $b . "\n";
-        }
-    }
-    return "PUSH COMPLETE";
-}
-
-function doRollback($destination)
-{
-    foreach (ip as $target => $ip) {
-        if (endsWith($target, $destination)) {
-            $layer = strtok($target, '-');
-            echo "Rolling back " . $target . " to previous version...\n";
-            $b = rollback($layer, $destination);
-
-            echo $b . "\n";
-        }
-    }
-    return "ROLLBACK COMPLETE";
+    // lookup username in databas
+    // check password
+    return true;
+    //return false if not valid
 }
 
 function requestProcessor($request)
 {
-    echo "received request" . PHP_EOL;
-    var_dump($request);
-    
-    
-    if (!isset($request['type'])) {
-        return "ERROR: unsupported message type";
-    }
-    switch ($request['type']) {
-        case "bundle":
-            return doBundle($request['source'], $request['version']);
-        case "push":
-            return doPush($request['destination'], $request['version']);
-        case "rollback":
-            return doRollback($request['layer']);
-    }
-    //log_message($request);
-    return array(
-        "returnCode" => '0',
-        'message' => "Server received request and processed"
-    );
+  echo "received request".PHP_EOL;
+  var_dump($request);
+  if(!isset($request['type']))
+  {
+    return "ERROR: unsupported message type";
+  }
+  switch ($request['type'])
+  {
+    case "login":
+      return doLogin($request['username'],$request['password']);
+    case "validate_session":
+      return doValidate($request['sessionId']);
+  }
+  return array("returnCode" => '0', 'message'=>"Server received request and processed");
 }
 
+$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
 
-$server = new rabbitMQServer("RabbitMQ.ini", "testServer");
-echo "testRabbitMQServer BEGIN" . PHP_EOL;
+echo "testRabbitMQServer BEGIN".PHP_EOL;
 $server->process_requests('requestProcessor');
-echo "testRabbitMQServer END" . PHP_EOL;
+echo "testRabbitMQServer END".PHP_EOL;
 exit();
 ?>
+
